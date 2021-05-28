6b1eb671962508445ae5d171bb7651f276daa7fe
vokda <djing_djang@hotmail.com>
2021-05-29 01:09:31 +0200

    several neat things
    - signal handling for IPC (SIGUSR1)
    - update time correction (still misses some updates)
    - added formating for text shown
    - error handling

--- bar.pl
+++ bar.pl
@@ -1,6 +1,6 @@
 #!/usr/bin/perl
 
-# TODO update time = -1 -> wait for signal SIGUSR1 containing something like {section_id => full_text}
+# TODO update-time = 0 -> wait for signal SIGUSR1 
 
 use warnings;
 use strict;
@@ -11,15 +11,22 @@ use File::Basename;
 # section_name => {full_text, update_time}
 my %sections;
 my $sh_dir = dirname(__FILE__);
+#my $ongoing = 0; # if signal occurs while ongoing = 1 erase old work and redo
 
-add_section('keyboard_layout', 'keyboard_layout.sh', update_time => 4, format => 'KBD[%]');
-#add_section('keyboard_layout', 'KBD [' . 'setxkbmap -query | sed -En "s/layout:\s+(..)/\1/p"' . ']', 10);
+add_section('keyboard_layout', 'keyboard_layout.sh', update_time => 0, format => 'KBD[%]');
 add_section('loadavg', 'loadavg.sh', update_time => 5);
-add_section('vpn', 'is_on_vpn.sh', update_time => 600, format => 'On VPN: %');
-add_section('date_time', "date_time.sh");
+add_section('vpn', 'is_on_vpn.sh', update_time => 0, format => 'On VPN: %');
+add_section('date_time', "date_time.sh", update_time => 0);
 add_section('gpu_temp', "gpu_temp.sh", update_time => 5, format => 'GPU %c');
 add_section('cpu_temp', "cpu_temp.sh", update_time => 5, format => 'CPU Cores 0[%c] 1[%c] 2[%c] 3[%c]');
 
+# signal handling for IPC
+sub handle_signal
+{
+	loop(1);
+}
+
+$SIG{USR1} = \&handle_signal;
 
 # don't buffer output. Print outs are messed with if sleep is used.
 $| = 1;
@@ -29,26 +36,43 @@ print "{ \"version\": 1 }\n" or die;
 print "[\n";
 print "[],\n";
 
-while(1)
+# just update everything once
+loop(1);
+
+while(1) 
 {
-	print '[';
+	loop();
+}
 
-	update();
-
-	to_json(qw(vpn keyboard_layout cpu_temp gpu_temp loadavg date_time));
-
-	print "],\n";
+# this is since the loop will be repeated twice
+# first time is simply to set all values once 
+# so no section is left empty 
+sub loop
+{
+	my $bar_text = '';
+	$bar_text = '[';
+	update(shift);
+	$bar_text .= to_json(qw(vpn keyboard_layout cpu_temp gpu_temp loadavg date_time));
+	$bar_text .=  "]";
+	# check json 
+	`echo '$bar_text' | jq; echo $?` ;
+	if ($?)
+	{
+		$bar_text = "[\"name\":\"error\", \"full_text\":\"$?\"]";
+	}
+	$bar_text .= ",\n";
+	print $bar_text;
 	sleep 1;
 }
 
 sub update
 {
+	my $override = shift;
 	for my $k (keys %sections)
 	{
-		my $update_time = $sections{$k}->{update_time};
+		my $update_time = $sections{$k}->{update_time} // 0;
 		my $time_since_update = $sections{$k}->{time_since_update};
-		die Dumper $k, \$sections{$k} unless $update_time;
-		if($update_time == 1 or $time_since_update > $update_time) 
+		if( ($time_since_update > $update_time ) or $override) 
 		{
 			my $cmd = $sections{$k}->{cmd};
 			my $result = sh($cmd, $sections{$k}->{is_sh});
@@ -74,7 +98,7 @@ sub to_json
 
 		push(@{$secs}, $ft);
 	}
-	print(join(',', @$secs));
+	return join(',', @$secs);
 }
 
 sub add_section
@@ -108,7 +132,7 @@ sub sh
 sub format_full_text
 {
 	my ($full_text, $format) = @_;
-	if($format)
+	if($format and $full_text)
 	{
 		my $c = () = $format =~ /%/g; # count number of %
 		if($c > 1)
@@ -131,3 +155,4 @@ sub get_weather
 	my $time = localtime;
 	#if ($time > 
 }
+
